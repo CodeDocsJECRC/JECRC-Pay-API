581cbe9ba5f1fb8348b0f4c09e810bb46708c306
maddhruv <dhruvjainpenny@gmail.com>
2018-10-01 18:17:30 +0530

    helmet and morgan setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,26 @@
 const restify = require('restify')
 const chalk = require('chalk')
+const helmet = require('helmet')
+const morgan = require('morgan')
 
 const server = restify.createServer()
+
+server.use(helmet.noCache())
+server.use(helmet.ieNoOpen())
+server.use(helmet.noSniff())
+server.use(helmet.xssFilter())
+server.use(helmet.frameguard({
+  action: 'deny'
+}))
+
+server.use(morgan(`- >\
+  ${chalk.blue(':date[web]')}\
+  ${chalk.yellow(':method')}\
+  ${chalk.white.bgRed(':url')}\
+  ${chalk.cyan(':remote-addr')}\
+  ${chalk.green(':response-time[3]')}\
+  ${chalk.magenta(':status')}\
+`))
 
 server.listen(process.env.PORT, (err) => {
   if (err) throw err
